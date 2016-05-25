b3baf3e66788066113e479fc285d93e8b3794be5
Azioth <aziothblack@gmail.com>
2016-05-25 02:38:26 -0300

    Added update bug info resource

--- korriban-api/controllers/testMethodBugController.js
+++ korriban-api/controllers/testMethodBugController.js
@@ -1,7 +1,22 @@
 var testMethodBugController = function(Suite){
 
   var patch = function(req, res) {
-    res.json({ "testMethodBugController": true})
+    Suite.find({ "suite_tests.test_methods._id": req.body.testmethod_id}, function(err, suites){
+      var suite = suites[0]
+      suite.suite_tests[0].test_methods.forEach(function(element, index, array){
+        if (element._id == req.body.testmethod_id){
+          suite.suite_tests[0].test_methods[index].testmethod_analysis = "BUG";
+          suite.suite_tests[0].test_methods[index].testmethod_bug_url = req.body.bug_url;
+        }
+      })
+      suite.save(function(err){
+        if (err) {
+          res.status(400).json(err);
+        } else {
+          res.status(204).send();
+        }
+      })
+    })
   }
 
   return {
