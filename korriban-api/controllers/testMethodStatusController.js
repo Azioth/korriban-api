46f827de18fb3fda8a038fb58f1e1eb9241b1d46
Azioth <aziothblack@gmail.com>
2016-05-25 02:38:41 -0300

    Added update status resource

--- korriban-api/controllers/testMethodStatusController.js
+++ korriban-api/controllers/testMethodStatusController.js
@@ -1,7 +1,21 @@
 var testMethodStatusController = function(Suite){
 
   var patch = function(req, res) {
-    res.json({ "testMethodStatusController": true})
+    Suite.find({ "suite_tests.test_methods._id": req.body.testmethod_id}, function(err, suites){
+      var suite = suites[0]
+      suite.suite_tests[0].test_methods.forEach(function(element, index, array){
+        if (element._id == req.body.testmethod_id){
+          suite.suite_tests[0].test_methods[index].testmethod_analysis = req.body.status
+        }
+      })
+      suite.save(function(err){
+        if (err) {
+          res.status(400).json(err);
+        } else {
+          res.status(204).send();
+        }
+      })
+    })
   }
 
   return {
