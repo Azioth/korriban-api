313a10f039ea373e32c43d798182cb795810b986
Sebastian Sandri <ssandri@velocitypartners.net>
2016-06-13 13:49:02 -0300

    Merged

--- routes/index.js
+++ routes/index.js
@@ -20,12 +20,7 @@ router.get('/korriban-api/v1/suites/:suiteId/tests', test.getTestInfo);
 */
 router.get('/korriban-api/v1/suites/:suiteId/tests/:testId/testMethods', testMethods.getTestMethods);
 router.get('/korriban-api/v1/testMethodHistory', testMethods.getHistory);
-<<<<<<< HEAD
 router.patch('/korriban-api/v1/testMethodStatus', testMethods.updateStatus);
 router.patch('/korriban-api/v1/bugInfo', testMethods.updateBugInfo);
-=======
-router.get('/korriban-api/v1/testMethodStatus', testMethods.updateStatus);
-router.get('/korriban-api/v1/updateBugInfo', testMethods.updateBugInfo);
->>>>>>> 24e02b13b4a2f2b8f5649a040de3caa7de74112e
 
 module.exports = router;
